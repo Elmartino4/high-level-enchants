c2c3bf54b234edf0161e3f7db35a79d31bf8dfe4
Elmartino4 <mart.baj2005@gmail.com>
2021-07-11 12:46:25 +1000

    .

--- src/main/java/net/fabricmc/example/mixin/EnchantLootMixin.java
+++ src/main/java/net/fabricmc/example/mixin/EnchantLootMixin.java
@@ -1,7 +1,6 @@
 package net.fabricmc.example.mixin;
 
 import net.minecraft.loot.function;
-
 import java.util.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
