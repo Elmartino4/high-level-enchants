e1ec9d95194727c2b7e6b9c2b99968bf212059d7
Elmartino4 <mart.baj2005@gmail.com>
2021-07-19 16:25:32 +1000

    added config files, changed modid

---
+++ src/main/java/com/github/Elmartino4/limtless2/config/ConfigInstance.java
@@ -0,0 +1,21 @@
+package com.github.Elmartino4.limitless2.config;
+
+public class ConfigInstance {
+  public int TravelSpeed;
+  public int MiningSpeed;
+  public int Luck;
+  public int Attack;
+  public int Protection;
+  public int ToolOther;
+  public int ArmourOther;
+
+  public ConfigInstance() {
+    TravelSpeed = 20;
+    MiningSpeed = 420;
+    Luck = 160;
+    Attack = 420;
+    Protection = 420;
+    ToolOther = 420;
+    ArmourOther = 420;
+  }
+}
