4e7ba5ffc0e3046d43884d51b34dd3a87cb7a3f0
Elmartino4 <mart.baj2005@gmail.com>
2021-07-24 12:52:35 +1000

    Update SetMaxLevel.java

--- src/main/java/com/github/Elmartino4/limtless2/SetMaxLevel.java
+++ src/main/java/com/github/Elmartino4/limtless2/SetMaxLevel.java
@@ -1,13 +1,14 @@
 package com.github.Elmartino4.limitless2;
 
 import com.github.Elmartino4.limitless2.config.ModConfig;
+import net.minecraft.enchantment.Enchantment;
 
 public class SetMaxLevel {
 	public int getMaxLevel(Enchantment ench) {
     System.out.println("Some indicator....." + ench.getTranslationKey());
     String[] categoryList = {"TravelSpeed", "MiningSpeed", "Luck", "Attack", "Protection", "ToolOther", "ArmourOther"};
-    for(category : categoryList){
-      if(ModConfig.INSTANCE.enchantCategories.["category"].contains(ench.getTranslationKey()))
+    for(String category : categoryList){
+      if(ModConfig.INSTANCE.enchantCategories["category"].contains(ench.getTranslationKey()))
         return ModConfig.INSTANCE["category"];
     }
     return ench.getMaxLevel();
